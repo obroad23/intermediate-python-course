b971e13284dee2a1b5e5a4bb76c15b10f68afa1f
obroad23 <o.broad@ucl.ac.uk>
2021-11-25 16:14:02 +0000

    roll two dice and sum the total

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,20 @@
 def main():
-  print('You rolled a die')
+#import all packages at the beginning of the script
+  import random
+
+  dice_rolls = 2
+  dice_sum = 0
+
+  #level of indentation defines what is and is not in the loop
+  for i in range(0,dice_rolls):
+    
+    #the beginning and the end of rhe (1,6) range are inclusive
+    roll = random.randint(1,6)
+    dice_sum = dice_sum + roll
+    print(f'You rolled a {roll}')
+
+  print(f'Your dice roll total is {dice_sum}')
+
 
 if __name__== "__main__":
   main()
